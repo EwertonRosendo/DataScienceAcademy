5fd38e00d4ae0896d7e3d6db95f2cf5025432201
EwertonRosendo <ewerton.rosendo15@gmail.com>
2022-05-05 11:05:43 -0300

    Atualização no exercicio 01

--- cap4_exercicios/ex001.py
+++ cap4_exercicios/ex001.py
@@ -1,5 +1,14 @@
+"""
+A forma com que eu fiz
+
 elementos = [1, 2, 4]
 def potencia(valor):
     return valor**3
 
 print(list(map(potencia, elementos)))
+"""
+
+# Forma da DSA
+elementos = [3, 4, 5]
+resultado = [valor**3 for valor in elementos]
+print(resultado)
--- cap4_exercicios/ex10.py
+++ cap4_exercicios/ex10.py
@@ -2,4 +2,3 @@ lista = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 for i, l in enumerate(lista):
     if i > 5:
         print(f"indice: {i}, letra:{l}")
-        
