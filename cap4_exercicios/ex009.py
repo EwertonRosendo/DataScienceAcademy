3966c78ece030981ed8aa6e8a3097a8abd2e4af3
EwertonRosendo <ewerton.rosendo15@gmail.com>
2022-05-05 10:56:13 -0300

    a

---
+++ cap4_exercicios/ex009.py
@@ -0,0 +1,4 @@
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'c': 4, 'd': 5}
+
+#dict3 = [{dict1.keys()]
---
+++ cap4_exercicios/ex10.py
@@ -0,0 +1,5 @@
+lista = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+for i, l in enumerate(lista):
+    if i > 5:
+        print(f"indice: {i}, letra:{l}")
+        
