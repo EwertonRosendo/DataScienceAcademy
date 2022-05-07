f48f21ca37f60bcf17a04d345d6b1c10a5460a75
EwertonRosendo <ewerton.rosendo15@gmail.com>
2022-05-07 14:56:36 -0300

    add

---
+++ visualgMoral.py
@@ -0,0 +1,57 @@
+"""# mod == (%)
+mod = 3 % 2
+print(mod)
+
+
+# No portugol o sinal de diferente de é simbolizado por essa merda aqui: (<>), no py py é por (!=)
+
+a = 1
+b = 1"""
+
+""""
+se(if) a =(==) b faca(:)
+    escreva("bom dia more")
+a = 1
+b = 2
+
+se a = b faca
+    
+    print("amanha tem prova")
+    
+    senao se a > b faca
+        print ("a é maior que b") 
+        
+        senao se a < b faca
+            print("a é menor que b")       
+    
+
+caractere == LETRA vem dentro de "", ou ''
+inteiro == NAO TEM VIRGULA. 1, 2, 3. 
+
+4
+
+real  == TEM VIRGULA
+logico == verdadeiro ou falso
+
+
+    
+
+     maior que é >, menor que <
+"""
+"""a = 1
+b = 2
+if a == b:
+    print("bom dia")
+    
+    caractere == str == texto
+
+"""
+"""frase = 4.0
+
+print(type(frase))
+"""
+a = 5
+b = 6
+c = a*b
+c = -c
+print(type(c))
