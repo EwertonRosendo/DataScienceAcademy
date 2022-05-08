b0e410c1912fb13cb556986482f6b5ec68e5f49b
EwertonRosendo <ewerton.rosendo15@gmail.com>
2022-05-08 14:07:18 -0300

    Update aulaModulos.py

--- aulaModulos.py
+++ aulaModulos.py
@@ -1,5 +1,23 @@
 class Circulo():
 
-    pi = 5
-    def __init__(self, raio=5):
+    pi = 3.14
+
+    def __init__(self, raio=4):
         self.raio = raio
+
+    def area(self):
+        return (self.raio*self.raio) * Circulo.pi
+
+    def setRaio(self, novo_raio):
+        self.raio = novo_raio
+
+    def getRaio(self):
+        return self.raio
+
+circ = Circulo()
+#  Raio antigo
+print(f"O raio é: {circ.getRaio()}")
+#  Alterando o raio
+circ.setRaio(5)
+#  Area com o novo raio
+print(f"A area é: {circ.area()}")
