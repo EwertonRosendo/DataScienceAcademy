daea01b31f2efd0e12ae6c983ebcfba7972c7f80
Rosendo <ewerton.rosendo15@gmail.com>
2022-06-20 17:41:37 -0300

    Update Instrucoes_Insert.py

--- Manipulando_Dados/Instrucoes_Insert.py
+++ Manipulando_Dados/Instrucoes_Insert.py
@@ -10,3 +10,16 @@ conn = sqlite3.connect("dsa.db")
 # criando um cursor para usar no bd
 c = conn.cursor()
 
+
+def create_table():
+    c.execute("CREATE TABLE IF NOT EXISTS produtos(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, date TEXT, "\
+              "prod_name TEXT, valor REAL)")
+
+def data_insert():
+    c.execute("INSERT INTO produtos VALUES(10, '2018-05-02 14:32:11', 'Teclado', 90 )")
+    conn.commit()
+    c.close()
+    conn.close()
+
+create_table()
+data_insert()
