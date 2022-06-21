0bb50d75aa2c1f1fdb1356f2974145fa95ee0b7a
Rosendo <ewerton.rosendo15@gmail.com>
2022-06-21 15:07:23 -0300

    inserindo try nas funções

--- Manipulando_Dados/Meu_Mini_Projeto.py
+++ Manipulando_Dados/Meu_Mini_Projeto.py
@@ -7,11 +7,27 @@ while True:
     print(f"[2] - Pesquisar na tabela")
     print(f"[3] - Deletar na tabela")
     print(f"[4] - Alterar na tabela")
+    print(f"[5] - Sair")
     duvida = int(input("Informe qual ação deseja realizar: \n"))
-
+    # A opção [1] serve para criar a tabela Pessoa que possui id, nome, trabalho e cpf
     if duvida == 1:
         modulos.funções.criar_tabela(c)
-    break
+
+    # A opção [2] serve para buscar algum dado na tabela
+    elif duvida == 2:
+        print()
+
+    # A opção [3] serve para deletar algum dado da tabela
+    elif duvida == 3:
+        print()
+
+    # A opção [4] serve para alterar algum dado na tabela
+    elif duvida == 4:
+        print()
+
+    # A opção [5] serve para finalizar o teste
+    elif duvida == 5:
+        break
 
 
 c.close()
--- Manipulando_Dados/modulos/funções.py
+++ Manipulando_Dados/modulos/funções.py
@@ -1,5 +1,14 @@
 import sqlite3
 # Função de criar tabela sendo passado como parametro o nome do cursor
 def criar_tabela(cursor):
-    cursor.execute("create table pessoa(id integer primary key autoincrement not null, nome varchar(140)," \
-              " trabalho varchar(40), cpf varchar(11))")
+    try:
+        cursor.execute("create table pessoa(id integer primary key autoincrement not null, nome varchar(140)," \
+                  " trabalho varchar(40), cpf varchar(11))")
+    except Exception as causa:
+        print(f"Detectamos uma falha e não conseguimos realizar a operação! causa: {causa.__class__}")
+
+    else:
+        print("Tabela criada com sucesso")
+
+    #finally:
+        #print()
