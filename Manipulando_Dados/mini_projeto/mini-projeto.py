a314c61b6015803bd333545f48cacc26379fca07
Rosendo <ewerton.rosendo15@gmail.com>
2022-07-03 18:17:49 -0300

    inicio

---
+++ Manipulando_Dados/mini_projeto/mini-projeto.py
@@ -0,0 +1,28 @@
+#Expressões regulares, modulo utilizado para processamento de texto,
+import re
+
+#Trabalhar com tempo ou datas
+import time
+
+#Manipular bancos de dados sqlite3 em python
+import sqlite3
+
+#Trabalhar com siglas de paises, ex: br = brasil
+import pycountry
+
+#numpy e pandas são bibliotecas feitas para manupulação de dados em python, normalmente aparecem juntos
+import numpy as np
+import pandas as pd
+
+#matplotlib e seboarn são para visualização de dados em python
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib import cm
+
+#sklearn é para aprendizado de maquina
+from sklearn.feature_extraction.text import CountVectorizer
+import warnings
+warnings.filterwarnings("ignore")
+
+#Definindo estilo dos graficos
+sns.set_theme(style="whitegrind")
